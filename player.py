41310736cc9fcbbe36de041b18455e5146530d97
toubal77 <72252709+toubal77@users.noreply.github.com>
2023-03-26 12:09:23 +0200

    create class player

---
+++ player.py
@@ -0,0 +1,10 @@
+class Player:
+    def __init__(self, name, company):
+        self.name = name
+        self.company = company
+
+    def buy_aircraft(self, aircraft):
+        print("player buying aircraft")
+
+    def sell_aircraft(self, aircraft):
+        print("player selling aircraft")
