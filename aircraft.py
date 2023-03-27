f0fe0231fc1651ad4fa0d2991246856d8656fd06
toubal77 <72252709+toubal77@users.noreply.github.com>
2023-03-27 14:51:58 +0200

    add somes commentary

--- aircraft.py
+++ aircraft.py
@@ -20,15 +20,21 @@ class Aircraft:
         cost = random.randint(100000, 1000000)
         aircraft.develop(cost)
         success_rate = random.randint(1, 10)
+        print(f"The aircraft was developed at a cost of {cost}$ and success rate of {success_rate}")
         if success_rate > 5:
+            cost_prod = random.randint(100000, 1000000)
+            aircraft.produce(cost_prod)
+            print(f"The aircraft have success rate of {success_rate} > 5. will be produced with cost of {aircraft.production_cost}$")
             aircraft.range += random.randint(1000, 5000)
-            aircraft.capacity += random.randint(10, 100)
+            aircraft.capacity += random.randint(100, 400)
             aircraft.fuel_consumption -= random.randint(1, 5)
-            aircraft.speed += random.randint(50, 200)
+            aircraft.speed += random.randint(400, 900)
 
     def is_competitive(self, competitor):
         if self.capacity > competitor.capacity and self.range > competitor.range and \
            self.fuel_consumption < competitor.fuel_consumption and self.speed > competitor.speed:
+            print("The aircraft was competitive")
             return True
         else:
+            print("The competitor was competitive")
             return False
--- player.py
+++ player.py
@@ -9,14 +9,18 @@ class Player:
         if self.cash >= aircraft.production_cost:
             self.cash -= aircraft.production_cost
             self.aircrafts.append(aircraft)
+            print(f"The aircraft was buyed from {self.company} with {aircraft.production_cost}$")
             return True
         else:
+            print(f"The company {self.company} does not have the necessary funds")
             return False
 
     def sell_aircraft(self, aircraft):
         if aircraft in self.aircrafts:
             self.cash += aircraft.production_cost // 2
             self.aircrafts.remove(aircraft)
+            print(f"The aircraft was selled from {self.company} with {aircraft.production_cost // 2}$")
             return True
         else:
+            print(f"The company {self.company} does not have this plane")
             return False
